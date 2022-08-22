104ea4baec59d67de111a031d83adff99583bc5b
David Lozano <dawgun@hotmail.com>
2022-08-22 14:59:51 +0200

    Changed class counter-* to counter__card

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,19 +1,19 @@
 const Counter = (): JSX.Element => {
   return (
     <div className="counter">
-      <div className="counter-days">
+      <div className="counter__card">
         <span className="counter__digit">02</span>
         <span className="counter__title">days</span>
       </div>
-      <div className="counter-hours">
+      <div className="counter__card">
         <span className="counter__digit">20</span>
         <span className="counter__title">hours</span>
       </div>
-      <div className="counter-minutes">
+      <div className="counter__card">
         <span className="counter__digit">34</span>
         <span className="counter__title">minutes</span>
       </div>
-      <div className="counter-seconds">
+      <div className="counter__card">
         <span className="counter__digit">09</span>
         <span className="counter__title">seconds</span>
       </div>
